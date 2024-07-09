8d377aee9f0e46ede90ab5c7589c15a256fc2975
rishiTamoghna17 <tamoghna171099@gmail.com>
2024-07-10 01:33:36 +0530

    try

--- src/config/index.ts
+++ src/config/index.ts
@@ -3,3 +3,5 @@ dotenv.config();
 const appRoot = process.cwd();
 export const FILE_SAVE_PATH = `${appRoot}/src`
 export const x_rapidapi_key = process.env["x-rapidapi-key"];
+export const IG_USERNAME = process.env.IG_USERNAME ||"";
+export const IG_PASSWORD = process.env.IG_PASSWORD ||"";
--- src/controller/media/instagram.ts
+++ src/controller/media/instagram.ts
@@ -1,40 +1,43 @@
 import axios from "axios";
 import { parse } from "node-html-parser";
 import cheerio from "cheerio";
-// const instagramPostLink = "https://www.instagram.com/p/CD5um-vHA33/";
 
+
+import { IgApiClient } from 'instagram-private-api';
+import puppeteer from 'puppeteer';
+import fs from 'fs';
+import { IG_PASSWORD, IG_USERNAME } from "../../config";
+// const instagramPostLink = "https://www.instagram.com/p/CD5um-vHA33/";
+const getVideoSrc = async(website:any,tab:any)=>{
+try{
+  await tab.goto(website);
+await tab.waitForSelector('video');
+return await tab.evaluate("document.querySelector('video').src");
+}catch(e){
+  console.log(e);
+}
+}
 export async function instagram(url: string) {
   try {
-    url = url + "embed" + "/captioned";
 
-    const response = await axios.get(url);
-    const responseData = response.data;
+    const browser = await puppeteer.launch({
+      headless: true,}); 
+      const [tab] = await browser.pages();
 
-    if (!responseData) {
-      throw new Error("No data received from Instagram.");
-    }
 
-    const root = parse(responseData);
-console.log("root:-", root);
-    let link = "";
-    if (responseData.search("video_url") !== -1) {
-      link = getVideoLinkFromHtml(responseData);
-    } else {
-      const imgElement = root.querySelector("img.EmbeddedMediaImage");
-      if (imgElement) {
-        link = imgElement.getAttribute("src") as any;
-      } else {
-        throw new Error("Image element not found in HTML.");
-      }
-    }
+   const src = await getVideoSrc(url,tab);
+   var reader = new FileReader();
+reader.onload = function() {
+    alert(reader.result);
+}
+const texts = reader.readAsText(src);
+console.log("src:-",texts);
+// Save the reel to a file
+// fs.writeFileSync('reel.mp4', reelBuffer);
 
-    while (link.search("&amp;") !== -1) {
-      link = link.replace("&amp;", "&");
-    }
 
-    const caption = await getCaptionFromHtml(responseData);
 
-    return { link, caption };
+
   } catch (e:any) {
     console.log("instagram error:-", e);
     return {
@@ -45,24 +48,59 @@ console.log("root:-", root);
   }
 }
 
-async function getCaptionFromHtml(html: any) {
-  const root = parse(html);
+// async function getCaptionFromHtml(html: any) {
+//   const root = parse(html);
 
-  let caption = root.querySelector(".Caption")?.text;
-  if (caption == undefined) caption = "No caption";
+//   let caption = root.querySelector(".Caption")?.text;
+//   if (caption == undefined) caption = "No caption";
 
-  caption = caption.replace("view all comments", "");
-  return caption;
-}
+//   caption = caption.replace("view all comments", "");
+//   return caption;
+// }
 
-function getVideoLinkFromHtml(html: any) {
-  let crop =
-    '{"' +
-    html.substring(html.search("video_url"), html.search("video_url") + 1000);
+// function getVideoLinkFromHtml(html: any) {
+//   let crop =
+//     '{"' +
+//     html.substring(html.search("video_url"), html.search("video_url") + 1000);
 
-  crop = crop.substring(0, crop.search(",")) + "}";
+//   crop = crop.substring(0, crop.search(",")) + "}";
 
-  return JSON.parse(crop).video_url;
-}
+//   return JSON.parse(crop).video_url;
+// }
 
+
+//
+//
+//
+//
+//     url = url + "embed" + "/captioned";
+
+//     const response = await axios.get(url);
+//     const responseData = response.data;
+
+//     if (!responseData) {
+//       throw new Error("No data received from Instagram.");
+//     }
+
+//     const root = parse(responseData);
+// console.log("root:-", root);
+//     let link = "";
+//     if (responseData.search("video_url") !== -1) {
+//       link = getVideoLinkFromHtml(responseData);
+//     } else {
+//       const imgElement = root.querySelector("img.EmbeddedMediaImage");
+//       if (imgElement) {
+//         link = imgElement.getAttribute("src") as any;
+//       } else {
+//         throw new Error("Image element not found in HTML.");
+//       }
+//     }
+
+//     while (link.search("&amp;") !== -1) {
+//       link = link.replace("&amp;", "&");
+//     }
+
+//     const caption = await getCaptionFromHtml(responseData);
+
+//     return { link, caption };
 
